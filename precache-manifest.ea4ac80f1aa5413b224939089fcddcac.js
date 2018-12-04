0f56fae9e22bd400ca4bc06f22896736f8689029
Vukasin Stojanovic <vultashin76@gmail.com>
2018-12-04 15:06:30 +0100

    Updates

---
+++ precache-manifest.ea4ac80f1aa5413b224939089fcddcac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7e70088cb587aebf352",
+    "url": "/RobotFriends/static/js/runtime~main.b7e70088.js"
+  },
+  {
+    "revision": "b2d97a5118aeea877ed4",
+    "url": "/RobotFriends/static/js/main.b2d97a51.chunk.js"
+  },
+  {
+    "revision": "340947e0cbcbe2d7d915",
+    "url": "/RobotFriends/static/js/1.340947e0.chunk.js"
+  },
+  {
+    "revision": "b2d97a5118aeea877ed4",
+    "url": "/RobotFriends/static/css/main.43a0f7e9.chunk.css"
+  },
+  {
+    "revision": "340947e0cbcbe2d7d915",
+    "url": "/RobotFriends/static/css/1.223ffb0b.chunk.css"
+  },
+  {
+    "revision": "c73631069e32db54642cadf6ed09be6e",
+    "url": "/RobotFriends/index.html"
+  }
+];
